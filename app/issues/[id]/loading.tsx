c718afca2e968dcc36df69483a25f260d4d27350
Shreeraam G <shreeram0312@gmail.com>
2024-05-02 21:29:51 +0530

    Add additional loading skeletons

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,5 +1,20 @@
+import { Box, Card, Flex } from "@radix-ui/themes";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
 const LoadingIssueDetailPage = () => {
-  return <div>Loading...</div>;
+  return (
+    <Box className="max-w-xl">
+      <Skeleton />
+      <Flex gapX="4" align="center" my="4">
+        <Skeleton width="5rem" />
+        <Skeleton width="8rem" />
+      </Flex>
+      <Card className="prose">
+        <Skeleton count={3} />
+      </Card>
+    </Box>
+  );
 };
 
 export default LoadingIssueDetailPage;
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -9,8 +9,6 @@ interface Props {
 }
 
 const IssueDetailPage = async ({ params }: Props) => {
-  // if (typeof params.id !== "number") notFound();
-
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
