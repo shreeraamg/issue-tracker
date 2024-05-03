2d2e3141f6e58f8ea6395732978255d0e26bd195
Shreeraam G <shreeram0312@gmail.com>
2024-05-03 07:02:46 +0530

    Change navbar layout

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -5,7 +5,7 @@ import { usePathname } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
 import classNames from "classnames";
 import { signIn, useSession, signOut } from "next-auth/react";
-import { Box, Button } from "@radix-ui/themes";
+import { Box, Button, Container, Flex } from "@radix-ui/themes";
 
 const Navbar = () => {
   const currentPath = usePathname();
@@ -17,34 +17,40 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="flex gap-x-6 border-b mb-5 px-5 h-14 items-center">
-      <Link href="/">
-        <AiFillBug />
-      </Link>
-      <ul className="flex gap-x-6">
-        {links.map((link) => (
-          <li key={link.href}>
-            <Link
-              className={classNames({
-                "text-zinc-900": link.href === currentPath,
-                "text-zinc-500": link.href !== currentPath,
-                "hover:text-zinc-800 transition-colors font-medium": true,
-              })}
-              href={link.href}
-            >
-              {link.label}
+    <nav className="border-b mb-5 px-5 h-14 py-3">
+      <Container>
+        <Flex justify="between">
+          <Flex align="center" gap="3">
+            <Link href="/">
+              <AiFillBug />
             </Link>
-          </li>
-        ))}
-      </ul>
-      <Box>
-        {status === "authenticated" && (
-          <Button onClick={() => signOut()}>Log out</Button>
-        )}
-        {status === "unauthenticated" && (
-          <Button onClick={async () => signIn()}>SignIn</Button>
-        )}
-      </Box>
+            <ul className="flex gap-x-6">
+              {links.map((link) => (
+                <li key={link.href}>
+                  <Link
+                    className={classNames({
+                      "text-zinc-900": link.href === currentPath,
+                      "text-zinc-500": link.href !== currentPath,
+                      "hover:text-zinc-800 transition-colors": true,
+                    })}
+                    href={link.href}
+                  >
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+          <Box>
+            {status === "authenticated" && (
+              <Button onClick={() => signOut()}>Log out</Button>
+            )}
+            {status === "unauthenticated" && (
+              <Button onClick={async () => signIn()}>SignIn</Button>
+            )}
+          </Box>
+        </Flex>
+      </Container>
     </nav>
   );
 };
