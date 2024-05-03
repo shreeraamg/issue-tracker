361caf0ef65c4a4dd6db4ccb14350cff6cd8c724
Shreeraam G <shreeram0312@gmail.com>
2024-05-04 01:53:07 +0530

    Refactor the navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,29 +1,13 @@
 "use client";
 
+import { Button, Container, Flex, Text } from "@radix-ui/themes";
+import classNames from "classnames";
+import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
-import classNames from "classnames";
-import { signIn, useSession, signOut } from "next-auth/react";
-import {
-  Avatar,
-  Box,
-  Button,
-  Container,
-  DropdownMenu,
-  Flex,
-  Text,
-} from "@radix-ui/themes";
 
 const Navbar = () => {
-  const currentPath = usePathname();
-  const { status, data: session } = useSession();
-
-  const links = [
-    { label: "Dashboard", href: "/" },
-    { label: "Issues", href: "/issues/list" },
-  ];
-
   return (
     <nav className="border-b mb-5 px-5 h-14 py-3">
       <Container>
@@ -32,39 +16,58 @@ const Navbar = () => {
             <Link href="/">
               <AiFillBug />
             </Link>
-            <ul className="flex gap-x-6">
-              {links.map((link) => (
-                <li key={link.href}>
-                  <Link
-                    className={classNames({
-                      "text-zinc-900": link.href === currentPath,
-                      "text-zinc-500": link.href !== currentPath,
-                      "hover:text-zinc-800 transition-colors": true,
-                    })}
-                    href={link.href}
-                  >
-                    {link.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+            <NavLinks />
           </Flex>
-          <Flex align="center" gapX="4">
-            {status === "authenticated" && (
-              <>
-                <Text>{session.user?.name}</Text>
-                <Button variant="soft" color="gray" onClick={() => signOut()}>
-                  Logout
-                </Button>
-              </>
-            )}
-            {status === "unauthenticated" && (
-              <Button onClick={async () => signIn()}>SignIn</Button>
-            )}
-          </Flex>
+          <AuthStatus />
         </Flex>
       </Container>
     </nav>
+  );
+};
+
+const NavLinks = () => {
+  const currentPath = usePathname();
+
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues/list" },
+  ];
+
+  return (
+    <ul className="flex gap-x-6">
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link
+            className={classNames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const AuthStatus = () => {
+  const { status, data: session } = useSession();
+
+  if (status === "loading") return null;
+
+  if (status === "unauthenticated")
+    return <Button onClick={async () => signIn()}>SignIn</Button>;
+
+  return (
+    <Flex align="center" gapX="4">
+      <Text>{session!.user!.name!}</Text>
+      <Button variant="soft" color="gray" onClick={() => signOut()}>
+        Logout
+      </Button>
+    </Flex>
   );
 };
 
