e388ee7e9d97e642142c6d00f74f5534c2756fd6
Shreeraam G <shreeram0312@gmail.com>
2024-05-03 06:11:55 +0530

    Remove duplicate skeletons

--- app/issues/[id]/EditIssueButton.tsx
+++ app/issues/[id]/EditIssueButton.tsx
@@ -6,7 +6,7 @@ const EditIssueButton = ({ issueId }: { issueId: number }) => {
   return (
     <Button>
       <MdOutlineEdit size={16} />
-      <Link href={`/issues/${issueId}/edit`}>Edit Issue</Link>
+      <Link href={`/issues/edit/${issueId}`}>Edit Issue</Link>
     </Button>
   );
 };
