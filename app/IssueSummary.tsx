b868e1faf6afcb9f9e244f3623d2aaa823af2c83
Shreeraam G <shreeram0312@gmail.com>
2024-05-04 12:28:30 +0530

    Build issue chart & summary component

---
+++ app/IssueSummary.tsx
@@ -0,0 +1,39 @@
+import { Status } from "@prisma/client";
+import { Card, Flex, Text } from "@radix-ui/themes";
+import Link from "next/link";
+
+interface Props {
+  open: number;
+  inProgress: number;
+  closed: number;
+}
+
+const IssueSummary = ({ open, inProgress, closed }: Props) => {
+  const statuses: { label: string; value: number; status: Status }[] = [
+    { label: "Open Issues", value: open, status: "OPEN" },
+    { label: "In-Progress Issues", value: inProgress, status: "IN_PROGRESS" },
+    { label: "Closed Issues", value: closed, status: "CLOSED" },
+  ];
+
+  return (
+    <Flex gap="4">
+      {statuses.map((status) => (
+        <Card key={status.status}>
+          <Flex direction="column" gap="1">
+            <Link
+              href={`/issues/list?status=${status.status}`}
+              className="text-sm font-medium"
+            >
+              {status.label}
+            </Link>
+            <Text size="6" className="font-bold">
+              {status.value}
+            </Text>
+          </Flex>
+        </Card>
+      ))}
+    </Flex>
+  );
+};
+
+export default IssueSummary;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
-import Image from "next/image";
-import Pagination from "./components/Pagination";
+import prisma from "@/prisma/client";
+import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
+import IssueChart from "./IssueChart";
 
-export default function Home() {
+export default async function Home() {
+  const open = await prisma.issue.count({ where: { status: "OPEN" } });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
+
   return (
     <div>
-      <LatestIssues />
+      <IssueChart open={open} closed={closed} inProgress={inProgress} />
     </div>
   );
 }
