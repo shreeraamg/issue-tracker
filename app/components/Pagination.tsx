fe6c6a35588d583e49b981dc96b87b75b1dfe87c
Shreeraam G <shreeram0312@gmail.com>
2024-05-04 11:30:22 +0530

    Implement pagination

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import { Button, Flex, Text } from "@radix-ui/themes";
+import { useSearchParams, useRouter } from "next/navigation";
 import {
   MdKeyboardDoubleArrowLeft,
   MdKeyboardArrowLeft,
@@ -13,7 +16,17 @@ interface Props {
 }
 
 const Pagination = ({ itemCount, pageSize, currentPage }: Props) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
   const pageCount = Math.ceil(itemCount / pageSize);
+
+  const changePage = (page: number) => {
+    const params = new URLSearchParams(searchParams);
+
+    params.set("page", page.toString());
+    router.push("?" + params.toString());
+  };
 
   if (pageCount <= 1) return null;
 
@@ -22,16 +35,36 @@ const Pagination = ({ itemCount, pageSize, currentPage }: Props) => {
       <Text size="2">
         Page {currentPage} of {pageCount}
       </Text>
-      <Button color="gray" variant="soft" disabled={currentPage === 1}>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === 1}
+        onClick={() => changePage(1)}
+      >
         <MdKeyboardDoubleArrowLeft />
       </Button>
-      <Button color="gray" variant="soft" disabled={currentPage === 1}>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === 1}
+        onClick={() => changePage(currentPage - 1)}
+      >
         <MdKeyboardArrowLeft />
       </Button>
-      <Button color="gray" variant="soft" disabled={currentPage === pageCount}>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(currentPage + 1)}
+      >
         <MdKeyboardArrowRight />
       </Button>
-      <Button color="gray" variant="soft" disabled={currentPage === pageCount}>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(pageCount)}
+      >
         <MdKeyboardDoubleArrowRight />
       </Button>
     </Flex>
