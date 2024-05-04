b4325f0170bfe23cbf405df6b14b06cf683c93c8
Shreeraam G <shreeram0312@gmail.com>
2024-05-04 09:08:31 +0530

    Enhance the API to assign issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,4 +1,4 @@
-import { issueSchema } from "@/app/validationSchemas";
+import { patchIssueSchema } from "@/app/validationSchemas";
 import { auth } from "@/auth";
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
@@ -12,9 +12,20 @@ export async function PATCH(
 
   const body = await request.json();
 
-  const validation = issueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
+
+  const { assignedToUserId, title, description } = body;
+
+  if (assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: { id: assignedToUserId },
+    });
+
+    if (!user)
+      return NextResponse.json({ error: "Invalid User" }, { status: 404 });
+  }
 
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
@@ -25,7 +36,7 @@ export async function PATCH(
 
   const updatedIssue = await prisma.issue.update({
     where: { id: issue.id },
-    data: { title: body.title, description: body.description },
+    data: { title, description, assignedToUserId },
   });
 
   return NextResponse.json(updatedIssue);
--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -4,5 +4,21 @@ export const issueSchema = z.object({
   title: z.string().min(1, "Title is required.").max(255),
   description: z
     .string({ message: "Description is required." })
-    .min(1, "Description is required."),
+    .min(1, "Description is required.")
+    .max(65535),
+});
+
+export const patchIssueSchema = z.object({
+  title: z.string().min(1, "Title is required.").max(255).optional(),
+  description: z
+    .string({ message: "Description is required." })
+    .min(1, "Description is required.")
+    .max(65535)
+    .optional(),
+  assignedToUserId: z
+    .string()
+    .min(1, "Assigned to userId is required")
+    .max(255)
+    .optional()
+    .nullable(),
 });
